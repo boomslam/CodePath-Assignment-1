7e145b03907f17c5bbc65caba15f5623096c3c15
Jess <jesswlam@gmail.com>
2016-02-06 00:37:50 -0800

    Added detail file view, swift view controller for detail file view, and added button states for favoriting files.

---
+++ CP Assignment 1/FileDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FileDetailViewController.swift
+//  CP Assignment 1
+//
+//  Created by Jess Lam on 2/6/16.
+//  Copyright © 2016 Jess Lam. All rights reserved.
+//
+
+import UIKit
+
+class FileDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onButton(sender: UIButton) {
+        sender.selected = true
+    }
+   
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
